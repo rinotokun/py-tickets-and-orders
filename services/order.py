ff4cc18dac93eec57882cb88d9794448244eda14
Sergey <rinoto_kun@gmail.com>
2026-06-24 00:31:56 +0300

    add user, order services and migrations

---
+++ services/order.py
@@ -0,0 +1,35 @@
+from django.db import transaction
+from django.contrib.auth import get_user_model
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket, MovieSession
+
+
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: str = None
+) -> None:
+    with transaction.atomic():
+        user = get_user_model().objects.get(username=username)
+        order = Order.objects.create(user=user)
+        if date:
+            order.created_at = date
+            order.save()
+        for ticket in tickets:
+            movie_session = MovieSession.objects.get(
+                id=ticket["movie_session"]
+            )
+            Ticket.objects.create(
+                movie_session=movie_session,
+                order=order,
+                row=ticket["row"],
+                seat=ticket["seat"]
+            )
+
+
+def get_orders(username: str = None) -> QuerySet[Order]:
+    queryset = Order.objects.all()
+    if username:
+        queryset = queryset.filter(user__username=username)
+    return queryset
