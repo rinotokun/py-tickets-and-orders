f049d15123ea043a3bc03c93484742f170b81ff6
Sergey <rinoto_kun@gmail.com>
2026-06-24 00:58:29 +0300

    Solution

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -8,8 +8,12 @@ def get_cinema_halls() -> QuerySet[CinemaHall]:
 
 
 def create_cinema_hall(
-    hall_name: str, hall_rows: int, hall_seats_in_row: int
+    hall_name: str,
+    hall_rows: int,
+    hall_seats_in_row: int
 ) -> CinemaHall:
     return CinemaHall.objects.create(
-        name=hall_name, rows=hall_rows, seats_in_row=hall_seats_in_row
+        name=hall_name,
+        rows=hall_rows,
+        seats_in_row=hall_seats_in_row
     )
--- services/order.py
+++ services/order.py
@@ -5,27 +5,27 @@ from django.db.models import QuerySet
 from db.models import Order, Ticket, MovieSession
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> None:
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = date
-            order.save()
-        for ticket in tickets:
-            movie_session = MovieSession.objects.get(
-                id=ticket["movie_session"]
-            )
-            Ticket.objects.create(
-                movie_session=movie_session,
-                order=order,
-                row=ticket["row"],
-                seat=ticket["seat"]
-            )
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = date
+        order.save()
+    for ticket in tickets:
+        movie_session = MovieSession.objects.get(
+            id=ticket["movie_session"]
+        )
+        Ticket.objects.create(
+            movie_session=movie_session,
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"]
+        )
 
 
 def get_orders(username: str = None) -> QuerySet[Order]:
